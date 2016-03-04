4ebf72b5e9279ddf3ef8e154792d7ce053128453
Sarah <trowbrsa@gmail.com>
2016-03-04 11:36:21 -0800

    Added container styling for each view and updated background image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,4 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
-  # def current_user
-  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  # end
-
-# helper_method :current_user
-
 end
