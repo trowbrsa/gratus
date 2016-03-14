4c5ed8f9d2037e5e7c1518c87eb141aff8eb33af
Sarah <trowbrsa@gmail.com>
2016-03-14 16:31:00 -0700

    Added additional styling to splash page

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -6,10 +6,8 @@ $( document ).ready(function() {
   $('.1grad').parent().css('background-color', 'rgba(37, 137, 211, 0.3)');
 });
 
-$( document ).ready(function() {
   $('.twotofourgrads').parent().css('background-color', 'rgba(37, 137, 211, 0.4)');
 });
 
-$( document ).ready(function() {
   $('.multigrad').parent().css('background-color', 'rgba(37, 137, 211, 0.6)');
 });
