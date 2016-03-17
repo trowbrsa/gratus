23168bf222c127e999b7dc79a0b8b39c729fa617
Sarah <trowbrsa@gmail.com>
2016-03-17 15:55:29 -0700

    Updated css

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
-  $('.1grad').parent().css('background-color', 'rgba(37, 137, 211, 0.3)');
+  $('.1grad').parent().css('background-color', 'rgba(37, 137, 211, 0.1)');
 
-  $('.twotofourgrads').parent().css('background-color', 'rgba(37, 137, 211, 0.4)');
+  $('.twotofourgrads').parent().css('background-color', 'rgba(37, 137, 211, 0.3)');
 
-  $('.multigrad').parent().css('background-color', 'rgba(37, 137, 211, 0.6)');
+  $('.multigrad').parent().css('background-color', 'rgba(37, 137, 211, 0.5)');
 });
